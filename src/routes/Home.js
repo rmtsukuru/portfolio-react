c20f357e4cf3c0a957e2bf41a9d3bba89fafbe06
Alice Nouhouemale <alicenouhouemale@gmail.com>
2023-01-18 20:35:01 -0800

    update

---
+++ src/routes/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "../components/navbar/Navbar";
+import HeroImg from "../components/home/HeroImg";
+import Footer from "../components/footer/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <HeroImg />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
